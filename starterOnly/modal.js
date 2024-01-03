7362f32143d3b9a0e93290a7768f1282a4b0acc0
das422 <simonin974@gmail.com>
2024-01-03 11:46:19 +0400

    test

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -23,7 +23,48 @@ function launchModal() {
 
 
 // close modal form
-close.addEventListener("click",closeModal);
+close.addEventListener("click", closeModal);
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+
+// Form entries implementation
+
+
+const form = document.getElementById("form");
+const submit = document.getElementById("submit");
+const firstname = document.getElementById("first");
+const lastname = document.getElementById("last");
+const eMail = document.getElementById("email");
+const birthDate = document.getElementById("birthdate");
+const quantitycompetition = document.getElementById("quantity");
+const loc1 = document.getElementById("location1");
+const loc2 = document.getElementById("location2");
+const loc3 = document.getElementById("location3");
+const loc4 = document.getElementById("location4");
+const loc5 = document.getElementById("location5");
+const loc6 = document.getElementById("location6");
+const error = document.getElementById("errorMessage");
+
+function validate( firstname) {
+  
+  if (firstname === '' && firstname.length > 2)
+  return true;
+}  {
+  return false
+  error.textContent = "notvalid";} 
+
+  console.log(firstname = 'bonjour')
+  
+
+
+
+
+
+
+
+
+
+
+
